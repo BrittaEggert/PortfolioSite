0ae844c8cd1d388e65dcdfae1ec345c137ae690f
BrittaEggert <britta@familyeggert.com>
2022-05-10 17:22:03 +0200

    created burger menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,6 @@
 console.log("Hi! Welcome to my portfolio page!")
+
+function menuToggle() {
+    const menuElement = document.getElementById('myNavtoggle');
+    menuElement.classList.toggle("responsive");
+}
