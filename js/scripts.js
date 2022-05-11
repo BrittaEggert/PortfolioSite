2d09855b81d8e9234b5cf5c6afe38a305c54666b
BrittaEggert <britta@familyeggert.com>
2022-05-11 10:09:20 +0200

    console.log deleted

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-console.log("Hi! Welcome to my portfolio page!")
-
 function menuToggle() {
     const menuElement = document.getElementById('myNavtoggle');
     menuElement.classList.toggle("responsive");
